afc436cda4fff1f27f034868c3407e5606ac879d
blind-coder-hubers <mark.hubers@alvaria.com>
2025-03-31 19:45:18 -0500

    Add first java to the script and testing it

--- build_tools/zulu_java_jdk_linux.sh
+++ build_tools/zulu_java_jdk_linux.sh
@@ -3,15 +3,18 @@
 # 
 #
 #  Run this script on a new linux system is to run the following command. You can pass arguments to the shell using the -s option:
-#   curl -Lfk https://github.com/alvaria-com/scrm_public_scripts/raw/refs/heads/main/build_tools/fortify_client_linux.sh | sudo bash -s <download|install|both>
+#   curl -Lfk https://github.com/alvaria-com/scrm_public_scripts/raw/refs/heads/main/build_tools/zulu_java_jdk_linux.sh | sudo bash -s <java_8|java_11|java_17|java_21|all>
 #
 # Created: Mar-31-2025 Hubers
 #-----------------------------------------------------------------------------------------------
 
-FORTIFY_FILE_URL='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/fortify/client/Fortify_SCA_24.4.0_linux_x64.run'
-FORTIFY_LICENSE_FILE_URL='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/fortify/license/fortify.license'
-TEMP_PATH='/tmp'
-INSTALL_PATH='/opt/fortify/sca_client'
+JAVA_08_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_jdk/zulu/zulu8.84.0.15-ca-jdk8.0.442-linux_x64.tar.gz'
+JAVA_11_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_jdk/zulu/zulu11.78.15-ca-jdk11.0.26-linux_x64.tar.gz'
+JAVA_17_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_jdk/zulu/zulu17.56.15-ca-jdk17.0.14-linux_x64.tar.gz'
+JAVA_21_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_jdk/zulu/zulu21.40.17-ca-jdk21.0.6-linux_x64.tar.gz'
+
+
+INSTALL_PATH='/usr/lib/jvm/zulu'
 
 
 # check if command line argument is empty or not present
@@ -27,55 +30,31 @@ echo "- Start of script and is doing task '$task'"
 echo "--------------------------------------------------------"
 echo " "
 
-### Download Fortify client files
-echo "Download Fortify client files for Linux OS..."
-if [[ "$task" =~ download|both ]]; then
-   HTTP_STATUS=$(curl -Is -o /dev/null -w "%{http_code}" "$FORTIFY_FILE_URL")
-   if [ "$HTTP_STATUS" -eq 200 ]; then
-      echo "  URL for $FORTIFY_FILE_URL exist. Let download it...";
-      curl -o $TEMP_PATH/fortify-client.run "$FORTIFY_FILE_URL"
-      chmod +x $TEMP_PATH/fortify-client.run
-   else
-      echo "-ERROR-  URL for FORTIFY_FILE_URL does NOT exist. Status code: $HTTP_STATUS";
-      exit 1;
-   fi
-   HTTP_STATUS=$(curl -Is -o /dev/null -w "%{http_code}" "$FORTIFY_LICENSE_FILE_URL")
-   if [ "$HTTP_STATUS" -eq 200 ]; then
-      echo "  URL for $FORTIFY_LICENSE_FILE_URL exist. Let download it...";
-      curl -o $TEMP_PATH/fortify-license "$FORTIFY_LICENSE_FILE_URL"
-   else
-      echo "-ERROR-  URL for FORTIFY_LICENSE_FILE_URL does NOT exist. Status code: $HTTP_STATUS";
-      exit 1;
-   fi
+## Basic function to download file from Nexus using curl and some error check.
+download_nexus_file() {
+    local url=$1
+    local output_path=$2
+
+    HTTP_STATUS=$(curl -Is -o /dev/null -w "%{http_code}" "$url")
+    if [ "$HTTP_STATUS" -eq 200 ]; then
+        echo "  URL for $url exists. Let's download it..."
+        curl -o "$output_path" "$url"
+        chmod +x "$output_path"
+    else
+        echo "-ERROR-  URL for $url does NOT exist. Status code: $HTTP_STATUS"
+        exit 1
+    fi
+}
+
+
+
+echo "--- Install Java 8   -----------------------------"
+if [[ "$task" =~ java_8|all ]]; then
+   mkdir -p $INSTALL_PATH/jdk_8/
+   download_nexus_file  "$JAVA_08_NEXUS_FILE"  "$INSTALL_PATH/jdk_8/java_jdk.tar.gz"
+   tar -xzvf $INSTALL_PATH/jdk_8/java_jdk.tar.gz -C $INSTALL_PATH/jdk_8
+   rm $INSTALL_PATH/jdk_8/java_jdk.tar.gz
+   echo "  Done installing Java 8 at $INSTALL_PATH/jdk_8"
 else
-    echo "  Task is not set to download fortify.  Skipping this step."
-fi
-
-
-echo "Install Fortify client for Linux..."
-if [[ "$task" =~ install|both ]]; then
-   ## Install client
-   $TEMP_PATH/fortify-client.run --mode unattended \
-      --fortify_license_path $TEMP_PATH/fortify-license \
-      --install_dir $INSTALL_PATH
-
-   ## Set some main settings for Fortify using a properties file
-   echo 'com.fortify.sca.limiters.MaxSink=256'.          >  $INSTALL_PATH/Core/config/fortify-sca.properties
-   echo 'com.fortify.sca.limiters.MaxSource=256'         >> $INSTALL_PATH/Core/config/fortify-sca.properties
-   echo 'com.fortify.sca.limiters.MaxNodesForGlobal=256' >> $INSTALL_PATH/Core/config/fortify-sca.properties
-   echo 'com.fortify.sca.hoa.Enable=false'               >> $INSTALL_PATH/Core/config/fortify-sca.properties
-   echo 'com.fortify.sca.Phase0HigherOrder.Level=0'      >> $INSTALL_PATH/Core/config/fortify-sca.properties
-   echo 'com.fortify.sca.Phase0HigherOrder.Languages=""' >> $INSTALL_PATH/Core/config/fortify-sca.properties
-
-   ## Update path to fortify cleint and make it work on reboots
-   echo "export PATH=${INSTALL_PATH}/bin:\$PATH" | sudo tee /etc/profile.d/fortify_env.sh > /dev/null
-
-   ## Updte the client with latest data from fortify site
-   ${INSTALL_PATH}/bin/fortifyupdate
-
-   ## Remove old installer files
-   rm -f $TEMP_PATH/fortify-client.run
-
-else
-    echo "  Task is not set to install fortify.  Skipping this step."
+    echo "  Not set to install this Java version.  Skipping this step."
 fi
