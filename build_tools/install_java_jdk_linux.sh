fd72eecb0594fb41833d3c4017f1f7507f92494c
blind-coder-hubers <mark.hubers@alvaria.com>
2025-04-07 19:21:44 -0500

    add part of ANT

--- build_tools/install_java_jdk_linux.sh
+++ build_tools/install_java_jdk_linux.sh
@@ -1,9 +1,8 @@
 #-----------------------------------------------------------------------------------------------
-#  Copy Azul Zulu Builds of OpenJDK to Linux host from Nexus
+#  Copy Azul Zulu of OpenJDK to Linux host from Nexus
 # 
-#
 #  Run this script on a new linux system is to run the following command. You can pass arguments to the shell using the -s option:
-#   curl -Lfk https://github.com/alvaria-com/scrm_public_scripts/raw/refs/heads/main/build_tools/install_java_jdk_linux.sh | sudo bash -s <java_8|java_11|java_17|java_21|all>
+#   curl -Lfk https://github.com/alvaria-com/scrm_public_scripts/raw/refs/heads/main/build_tools/install_java_jdk_linux.sh | sudo bash -s <java_8|java_11|java_17|java_21|ANT_110|all>
 #
 # Created: Mar-31-2025 Hubers
 # Updates: xxx
@@ -15,7 +14,12 @@ JAVA_17_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-p
 JAVA_21_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_jdk/zulu/zulu21.40.17-ca-jdk21.0.6-linux_x64.tar.gz'
 JAVA_05_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-iq-hosted/com/alvaria/thirdparty/java/jdk/java/1.5/java-1.5.tar.gz'
 
-INSTALL_PATH='/usr/lib/java/jdk'
+ANT_10_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_ant/apache-ant-1.10.15-bin.tar.gz'
+ANT_IVY_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_ant/apache-ivy-2.5.3-bin.tar.gz'
+ANT_CONTRIB_NEXUS_FILE='https://nexus.aws.alvaria.com/nexus/repository/alvaria-raw-prod-hosted/scrm/build_tools/java_ant/ant-contrib-1.0b3.jar'
+
+JDK_INSTALL_PATH='/usr/lib/java/jdk'
+ANT_INSTALL_PATH='/usr/lib/ant'
 
 # check if command line argument is empty or not present
 if [ -z $1 ]; then
@@ -30,8 +34,9 @@ echo "-- Start of Java JDKs install and is doing task: '$task'"
 echo ' '
 
 
-### Create the main folder that all java jdks will live in.
-sudo mkdir -p $INSTALL_PATH
+### Create the main folders.
+sudo mkdir -p $JDK_INSTALL_PATH
+sudo mkdir -p $ANT_INSTALL_PATH
 
 ## Basic function to download file from Nexus using curl and some error check.
 download_nexus_file() {
@@ -52,21 +57,22 @@ download_nexus_file() {
 
 echo "--- Install Java 5   -----------------------------"
     if [[ "$task" =~ java_5|all ]]; then
-    JAVA_INSTALL_PATH=$INSTALL_PATH/jdk_5
-    mkdir -p $JAVA_INSTALL_PATH
-    download_nexus_file  "$JAVA_05_NEXUS_FILE"  "$JAVA_INSTALL_PATH/java_jdk.tar.gz"
-    tar -xzf $JAVA_INSTALL_PATH/java_jdk.tar.gz  --strip-components=1 -C $JAVA_INSTALL_PATH
+    JAVA_JDK_INSTALL_PATH=$JDK_INSTALL_PATH/jdk_5
+    mkdir -p $JAVA_JDK_INSTALL_PATH
+    download_nexus_file  "$JAVA_05_NEXUS_FILE"  "$JAVA_JDK_INSTALL_PATH/java_jdk.tar.gz"
+    tar -xzf $JAVA_JDK_INSTALL_PATH/java_jdk.tar.gz  --strip-components=1 -C $JAVA_JDK_INSTALL_PATH
 
     ## Let create a file to show what the full version of this java is:
-    tar -tf $JAVA_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1 > $JAVA_INSTALL_PATH/java_version
+    tar -tf $JAVA_JDK_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1 > $JAVA_JDK_INSTALL_PATH/java_version
 
-    rm $JAVA_INSTALL_PATH/java_jdk.tar.gz
+    rm $JAVA_JDK_INSTALL_PATH/java_jdk.tar.gz
 
     ## Set an env to this for ref
-    echo "export JAVA_HOME_5=${JAVA_INSTALL_PATH}" | sudo tee /etc/profile.d/java_home_5_env.sh
-    echo "Created env: JAVA_HOME_5=${JAVA_INSTALL_PATH} for next bootup."
+    echo "export JAVA_HOME_5=${JAVA_JDK_INSTALL_PATH}" | sudo tee /etc/profile.d/java_home_5_env.sh
+    echo "Created env: JAVA_HOME_5=${JAVA_JDK_INSTALL_PATH} for next bootup."
+    echo "If you want to use this version of JDK, export JAVA_HOME=\$JAVA_HOME_5"
     
-    echo "Done installing Java 1.5 at ${INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
+    echo "Done installing Java 1.5 at ${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
     else
         echo "** Not set to install this Java version.  Skipping this step."
     fi
@@ -78,19 +84,20 @@ echo "--- Install Java 8   -----------------------------"
     JAVA_VER='08'
 
     NEXUS_FILE_VAR="JAVA_${JAVA_VER}_NEXUS_FILE"
-    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$INSTALL_PATH/java_jdk.tar.gz"
+    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$JDK_INSTALL_PATH/java_jdk.tar.gz"
 
     
-    tar -xzf $INSTALL_PATH/java_jdk.tar.gz -C $INSTALL_PATH
+    tar -xzf $JDK_INSTALL_PATH/java_jdk.tar.gz -C $JDK_INSTALL_PATH
     ## Let create a file to show what the full version of this java is:
-    JAVA_VERSION_FOLDER=$(tar -tf $INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
-    rm $INSTALL_PATH/java_jdk.tar.gz
+    JAVA_VERSION_FOLDER=$(tar -tf $JDK_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
+    rm $JDK_INSTALL_PATH/java_jdk.tar.gz
 
     ## Set an env to this for ref
-    echo "export JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
-    echo "Created env: JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "export JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
+    echo "Created env: JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "If you want to use this version of JDK, export JAVA_HOME=\$JAVA_HOME_${JAVA_VER}"
 
-    echo "Done installing Java $JAVA_VER at ${INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
+    echo "Done installing Java $JAVA_VER at ${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
     else
         echo "** Not set to install this Java version.  Skipping this step."
     fi
@@ -102,18 +109,19 @@ echo "--- Install Java 11   ----------------------------"
     JAVA_VER='11'
 
     NEXUS_FILE_VAR="JAVA_${JAVA_VER}_NEXUS_FILE"
-    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$INSTALL_PATH/java_jdk.tar.gz"
+    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$JDK_INSTALL_PATH/java_jdk.tar.gz"
 
-    tar -xvf $INSTALL_PATH/java_jdk.tar.gz -C $INSTALL_PATH
+    tar -xvf $JDK_INSTALL_PATH/java_jdk.tar.gz -C $JDK_INSTALL_PATH
     ## Let create a file to show what the full version of this java is:
-    JAVA_VERSION_FOLDER=$(tar -tf $INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
-    rm $INSTALL_PATH/java_jdk.tar.gz
+    JAVA_VERSION_FOLDER=$(tar -tf $JDK_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
+    rm $JDK_INSTALL_PATH/java_jdk.tar.gz
 
     ## Set an env to this for ref
-    echo "export JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
-    echo "Created env: JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "export JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
+    echo "Created env: JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "If you want to use this version of JDK, export JAVA_HOME=\$JAVA_HOME_${JAVA_VER}"
 
-    echo "Done installing Java $JAVA_VER at ${INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
+    echo "Done installing Java $JAVA_VER at ${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
     else
         echo "** Not set to install this Java version.  Skipping this step."
     fi
@@ -125,41 +133,71 @@ echo "--- Install Java 17   ----------------------------"
     JAVA_VER='17'
 
     NEXUS_FILE_VAR="JAVA_${JAVA_VER}_NEXUS_FILE"
-    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$INSTALL_PATH/java_jdk.tar.gz"
+    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$JDK_INSTALL_PATH/java_jdk.tar.gz"
 
-    tar -xzf $INSTALL_PATH/java_jdk.tar.gz -C $INSTALL_PATH
+    tar -xzf $JDK_INSTALL_PATH/java_jdk.tar.gz -C $JDK_INSTALL_PATH
     ## Let create a file to show what the full version of this java is:
-    JAVA_VERSION_FOLDER=$(tar -tf $INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
-    rm $INSTALL_PATH/java_jdk.tar.gz
+    JAVA_VERSION_FOLDER=$(tar -tf $JDK_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
+    rm $JDK_INSTALL_PATH/java_jdk.tar.gz
 
     ## Set an env to this for ref
-    echo "export JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
-    echo "Created env: JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "export JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
+    echo "Created env: JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "If you want to use this version of JDK, export JAVA_HOME=\$JAVA_HOME_${JAVA_VER}"
 
-    echo "Done installing Java $JAVA_VER at ${INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
+    echo "Done installing Java $JAVA_VER at ${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
     else
         echo "** Not set to install this Java version.  Skipping this step."
     fi
     echo ''
+
 
 echo "--- Install Java 21   ----------------------------"
     if [[ "$task" =~ java_21|all ]]; then
     JAVA_VER='21'
 
     NEXUS_FILE_VAR="JAVA_${JAVA_VER}_NEXUS_FILE"
-    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$INSTALL_PATH/java_jdk.tar.gz"
+    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$JDK_INSTALL_PATH/java_jdk.tar.gz"
 
-    tar -xzf $INSTALL_PATH/java_jdk.tar.gz -C $INSTALL_PATH
+    tar -xzf $JDK_INSTALL_PATH/java_jdk.tar.gz -C $JDK_INSTALL_PATH
     ## Let create a file to show what the full version of this java is:
-    JAVA_VERSION_FOLDER=$(tar -tf $INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
-    rm $INSTALL_PATH/java_jdk.tar.gz
+    JAVA_VERSION_FOLDER=$(tar -tf $JDK_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
+    rm $JDK_INSTALL_PATH/java_jdk.tar.gz
 
     ## Set an env to this for ref
-    echo "export JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
-    echo "Created env: JAVA_HOME_${JAVA_VER}=${INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "export JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}" | sudo tee /etc/profile.d/java_home_${JAVA_VER}_env.sh > /dev/null
+    echo "Created env: JAVA_HOME_${JAVA_VER}=${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER} for next bootup."
+    echo "If you want to use this version of JDK, export JAVA_HOME=\$JAVA_HOME_${JAVA_VER}"
 
-    echo "Done installing Java $JAVA_VER at ${INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
+    echo "Done installing Java $JAVA_VER at ${JDK_INSTALL_PATH}/${JAVA_VERSION_FOLDER}"
     else
         echo "** Not set to install this Java version.  Skipping this step."
+    fi
+    echo ''
+
+
+echo "--- Install ANT 1.10  ----------------------------"
+    if [[ "$task" =~ ant_110|all ]]; then
+    ANT_VER='10'
+
+    NEXUS_FILE_VAR="ANT_${ANT_VER}_NEXUS_FILE"
+
+    download_nexus_file "$(eval echo \$$NEXUS_FILE_VAR)"  "$ANT_INSTALL_PATH/ant.tar.gz"
+    download_nexus_file $ANT_IVY_NEXUS_FILE  "$ANT_INSTALL_PATH/ivy.tar.gz"
+    download_nexus_file $ANT_CONTRIB_NEXUS_FILE  "$ANT_INSTALL_PATH/ant.contrib.jar"
+
+
+    tar -xzf $ANT_INSTALL_PATH/ant.tar.gz -C $ANT_INSTALL_PATH
+    ANT_VERSION_FOLDER=$(tar -tf $ANT_INSTALL_PATH/java_jdk.tar.gz | awk -F/ '{print $1}' | uniq | head -n 1)
+    rm $ANT_INSTALL_PATH/java_jdk.tar.gz
+
+    ## Set an env to this for ref
+    echo "export ANT_HOME_${ANT_VER}=${ANT_INSTALL_PATH}/${ANT_VERSION_FOLDER}" | sudo tee /etc/profile.d/ant_home_${ANT_VER}_env.sh > /dev/null
+    echo "Created env: ANT_HOME_${ANT_VER}=${ANT_INSTALL_PATH}/${ANT_VERSION_FOLDER} for next bootup."
+    echo "If you want to use this version of ANT, export ANT_HOME=\$ANT_HOME_${ANT_VER}"
+
+    echo "Done installing ANT $ANT_VER at ${ANT_INSTALL_PATH}/${ANT_VERSION_FOLDER}"
+    else
+        echo "** Not set to install this ANT version.  Skipping this step."
     fi
     echo ''
